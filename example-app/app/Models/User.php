dac0d477f53be9426fbe13c4f4c57dc05f9eba18
Milica <m.pranjkic@gmail.com>
2022-04-04 14:02:52 +0200

    Creating new Eloquent model -Post with one-to-many relationship with -User

--- example-app/app/Models/User.php
+++ example-app/app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);   //this relation is added to be able to fatch users'n posts, user can have many posts
+    }
 }
